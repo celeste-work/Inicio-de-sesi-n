ed3e2ede0c675b65abca3325f45cdb0678f4796f
celeste-work <celesteaquino12@gmail.com>
2024-08-12 16:46:18 -0300

    Inicio de sesión

---
+++ recepciones.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es-ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/hojaDeEstilo.css">
+    <title>Recepción de la información</title>
+</head>
+<body>
+    
+<p>Bienvenido <?php echo($_POST["user"]); ?></p>
+
+</body>
+
+
+</body>
+</html>
